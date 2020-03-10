2e276524ce961c2fb479127894ea02e295e2b072
Leonardo <60203599+leonardopollesel@users.noreply.github.com>
2020-03-09 23:52:38 -0400

    added more descriptions

--- Assignment2.py
+++ Assignment2.py
@@ -7,75 +7,76 @@ import json
 names = list()
 purposes = list()
 
-# Take elements from .txt file
+# Take elements from WS1.txt file
 file1 = open("WS1.txt", "r").read()
 file1 = json.loads(file1)
 names.extend(list(file1.keys()))
 purposes.extend(list(file1.values()))
 
-
-# Take elements form .txt file
+# Take elements form WS2.txt file
 file2 = pd.read_csv("WS2.txt", sep=":", header=None)
 file2[1] = file2[1].str.rstrip(",Purpose")
 names.extend(list(file2[1]))
 purposes.extend(list(file2[2]))
 
-
-# Take elements form .csv file
+# Take elements form WS3.csv file
 file3 = pd.read_csv("WS3.csv")
 names.extend(list(file3["Name"]))
 purposes.extend(list(file3["Purpose"]))
 
-# Take elements from .txt file
+# Take elements from WS4.txt file
 file4 = pd.read_csv("WS4.txt", sep=":", header=None)
 names.extend(list(file4.iloc[0::2, 1]))
 purposes.extend(list(file4.iloc[1::2, 1]))
 
-# Take elements form .txt file
+# Take elements form WS5.txt file
 file5 = pd.read_csv("WS5.txt", sep=":", header=None)
 names.extend(list(file5.iloc[0:50, 1]))
 purposes.extend(list(file5.iloc[50:100, 1]))
 
-# Take elements form .txt file
+# Take elements form WS6.csv file
 file6 = pd.read_csv("WS6.csv")
 names.extend(list(file3["Name"]))
 purposes.extend(list(file3["Purpose"]))
 
-# Take elements form .txt file
+# Take elements form WS7.txt file
 file7 = pd.read_csv("WS7.txt", sep=":", header=None)
 names.extend(list(file7.iloc[0::2, 1]))
 purposes.extend(list(file7.iloc[1::2, 1]))
 
-# Take elements form .txt file
+# Take elements form WS8.txt file
 file8 = pd.read_csv("WS8.txt", sep="\n", header=None)
 file8[0] = file8[0].str.lstrip("0123456789) ")
 names.extend(list(file8.iloc[0::2, 0]))
 purposes.extend(list(file8.iloc[1::2, 0]))
 
-# Take elements form .txt file
+# Take elements form WS9.txt file
 file9 = pd.read_csv("WS9.txt", sep="\t+", engine="python")
 names.extend(list(file9["Name"]))
 purposes.extend(list(file9["Purpose"]))
 
-# Take elements form .csv file
+# Take elements form WS10.csv file
 file10 = pd.read_csv("WS10.csv")
 names.extend(list(file10["Name"]))
 purposes.extend(list(file10["Purpose"]))
 
 # create data frame with names and purposes
-
 finalData = pd.DataFrame(columns=["score", "names", "purposes"], index=range(0, 500))
 finalData["names"] = names
 finalData["purposes"] = purposes
 
+# create empty list to store scores from sentiment analysis
 score = []
 
+# perform sentiment analysis on purposes of companies
 for j in range(len(finalData["purposes"])):
     sentiment = analyzer().polarity_scores(finalData["purposes"][j])
     score.append(sentiment["compound"])
 
+# add score list to final data frame
 finalData["score"] = score
 
+# reorder companies in descending order
 finalData = finalData.sort_values(by="score", ascending=False)
 finalData.reset_index()
 finalData.drop("index", axis=True, inplace=True)
